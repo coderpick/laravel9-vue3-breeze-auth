869b33cb053d6f2286f3d46e5397426b8196440f
coderpick <coderpickbd@gmail.com>
2022-11-23 18:56:21 +0600

    setup done

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+   
+    <div class="wrapper">
+      <h1>Laravel 9 Vue 3 Breeze Api Auth</h1>
+    </div>
+  </header>
+
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+    display: flex;
+      align-items: center;
+      justify-content: center;
+}
+</style>
